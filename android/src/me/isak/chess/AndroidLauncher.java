ef1956bb879ab33648a425ed79a88d292bf73810
isaksolheim <isak@isak.me>
2024-02-21 18:30:52 +0100

    Initialize libgdx project

---
+++ android/src/me/isak/chess/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package me.isak.chess;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import me.isak.chess.Chess;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Chess(), config);
+	}
+}
