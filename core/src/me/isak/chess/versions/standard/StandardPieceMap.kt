0610056044623c22b6fa13ceac06a13633f2c69c
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-04-06 23:51:18 +0200

    make standard piecemap

---
+++ core/src/me/isak/chess/versions/standard/StandardPieceMap.kt
@@ -0,0 +1,29 @@
+package me.isak.chess.versions.standard
+
+import me.isak.chess.move.PieceMap
+import me.isak.chess.move.Piece
+import me.isak.chess.move.PieceDirector
+
+val pd = PieceDirector()
+
+val standardPieceMap: Map<Char, Piece> = mapOf(
+    'r' to pd.rook().buildPiece(),
+    'R' to pd.rook().buildPiece(),
+    'n' to pd.knight().buildPiece(),
+    'N' to pd.knight().buildPiece(),
+    'b' to pd.bishop().buildPiece(),
+    'B' to pd.bishop().buildPiece(),
+    'q' to pd.queen().buildPiece(),
+    'Q' to pd.queen().buildPiece(),
+    'k' to pd.bKing().buildPiece(),
+    'K' to pd.wKing().buildPiece(),
+    'P' to pd.wPawn().buildPiece(),
+    'p' to pd.bPawn().buildPiece()
+)
+class StandardPieceMap: PieceMap {
+
+    override fun getPiece(piece: Char): Piece { 
+        return standardPieceMap.get(piece) ?: throw Error("Piece does not exist for this piece :$piece")
+    }   
+}
+
