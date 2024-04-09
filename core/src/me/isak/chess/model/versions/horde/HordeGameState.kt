97785e4f5aef89a328855971be2de287e667ddf0
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-04-09 10:36:29 +0200

    fix proper horde setup (#29)

--- core/src/me/isak/chess/model/versions/horde/HordeGameState.kt
+++ core/src/me/isak/chess/model/versions/horde/HordeGameState.kt
@@ -3,12 +3,9 @@ package me.isak.chess.model.versions.horde
 import me.isak.chess.model.base.SimpleMoveCalculator
 import me.isak.chess.model.versions.standard.StandardGameState
 
-val default = "rnbqkbnrpppppppp         PP  PP PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"
-val testttt = "rnbqkbnrpppppppp                       P                        "
-
 class HordeGameState(moveCalculator: SimpleMoveCalculator) : StandardGameState(moveCalculator) {
 
     init {
-        setBoardAsString(testttt)
+        setBoardAsString("rnbqkbnrpppppppp         PP  PP PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP")
     }
 }
