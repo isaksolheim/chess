02477c59a494c151897678ab4249714897d4ced9
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-04-16 08:40:22 +0200

    write general stalemate test (#56)

--- core/test/me/isak/chess/integration/StandardChessTest.kt
+++ core/test/me/isak/chess/integration/StandardChessTest.kt
@@ -2,6 +2,7 @@ package me.isak.chess.integration
 
 import org.junit.jupiter.api.Test;
 import me.isak.chess.model.base.Game
+import org.junit.jupiter.api.Assertions.assertEquals;
 
 class StandardChessTest {
 
@@ -29,5 +30,22 @@ class StandardChessTest {
             TestState(13, "r1bqkbnr/1ppppQpp/8/p7/1nB1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4", empty),
         )
         integrationTestRunner.run(expectedStates, game)     
+    }
+    
+    @Test
+    fun drawByFiftyMoveRule() {
+        val game = Game("standard", fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 49 100")
+
+        var result = game.checkGameOver()
+        assertEquals(false, result.gameOver, "Game should not be over")
+        
+        val expectedStates: List<TestState> = listOf(
+            TestState(62, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 49 100", listOf(47, 45)),
+            TestState(45, "rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq - 50 100", empty),
+        )
+        integrationTestRunner.run(expectedStates, game)     
+        
+        result = game.checkGameOver()
+        assertEquals(true, result.gameOver, "Game should be over by 50 move rule")
     }
 }
