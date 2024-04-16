7c3aefd447d7a3e47cd8da47990c2536d275c4dd
isaksolheim <isak@isak.me>
2024-04-16 10:29:08 +0200

    Add GameView back button

--- core/src/me/isak/chess/views/GameScreenView.kt
+++ core/src/me/isak/chess/views/GameScreenView.kt
@@ -7,29 +7,54 @@ import me.isak.chess.Chess
 import me.isak.chess.Renderer
 import me.isak.chess.model.base.Game
 import me.isak.chess.viewmodels.GameViewModel
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 
 class GameScreen(private val app: Chess, private val game: Game) : ScreenAdapter() {
     private val spriteBatch by lazy { Renderer.spriteBatch }
-
+    private val stage = Stage(ScreenViewport())
     private val gameViewModel = GameViewModel(game, app)
     private val boardView = BoardView(gameViewModel)
 
+    init {
+        setupUI()
+    }
+
+    private fun setupUI() {
+        Gdx.input.inputProcessor = stage
+        val backButton = TextButton("Back", app.skin)
+        backButton.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent, actor: Actor) {
+                app.setScreen(MainMenuView(app))
+            }
+        })
+
+        val table = Table()
+        table.setFillParent(true)
+        table.add(backButton).expand().top().left().pad(100f)
+
+        stage.addActor(table)
+    }
+
     override fun show() {
-        Gdx.input.inputProcessor = boardView
+        Gdx.input.inputProcessor = stage
     }
 
     override fun render(delta: Float) {
         ScreenUtils.clear(0.95f, 0.95f, 0.95f, 1f)
 
+        // Render the board view separately
         spriteBatch.begin()
         boardView.render()
         spriteBatch.end()
 
-        // Check if the game is over and navigate to a new screen if it is
-        val (gameOver, message) = gameViewModel.checkGameOver()
-        if (gameOver) {
-            app.setScreen(GameOverScreenView(app, game, message))
-        }
+        // Update and draw the stage for UI
+        stage.act(delta)
+        stage.draw()
     }
 
     override fun hide() {
@@ -38,5 +63,6 @@ class GameScreen(private val app: Chess, private val game: Game) : ScreenAdapter
 
     override fun dispose() {
         spriteBatch.dispose()
+        stage.dispose()
     }
 }
